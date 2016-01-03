01546b4f1ef2e844328f11c40536e3dc13aa6304
John Potess <jppotess@gmail.com>
2016-01-02 23:55:18 -0600

    babel into gulpflow for es6

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var sass        = require('gulp-sass');
 var jade        = require('jade');
 var gulpJade    = require('gulp-jade');
 var phpJade     = require('gulp-jade-php');
-// var jshint      = require('gulp-jshint');
+var babel       = require("gulp-babel");
+var jshint      = require('gulp-jshint');
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
 var plumber     = require('gulp-plumber');
@@ -19,6 +20,17 @@ var plumberErrorHandler = { errorHandler: notify.onError({
 };
 
 
+// var src = {
+//     scss:   '/stylesheets/**/*.scss',
+//     cssdist: '/dist/css',
+//     js:     '/javascript/**/*.js',
+//     jsdist: '/dist/js',    
+//     // html:   '/',
+//     php:     '/*.php',
+//     templates:    '/templates/**/*.jade',
+//     templatesDist: '/testywesty'
+// };
+
 // Jade to PHP
 gulp.task('jade', function() {
      gulp.src('src/templates/**/*.jade')
@@ -31,33 +43,6 @@ gulp.task('jade', function() {
         .pipe(reload({stream: true}));
 });
 
-
-
-// var src = {
-//     scss:   '/stylesheets/**/*.scss',
-//     cssdist: '/dist/css',
-//     js:     '/javascript/**/*.js',
-//     jsdist: '/dist/js',    
-//     // html:   '/',
-//     php:     '/*.php',
-//     templates:    '/templates/**/*.jade',
-//     templatesDist: '/testywesty'
-// };
-
-// Proxy Server + watching scss/php/js files
-gulp.task('serve', function() {
-
-    // browserSync({
-    //     proxy: "www.jp-gen-starter-bp.dev",
-    //     // tunnel: "genesis-bourbon"
-    // });
-
-    gulp.watch('src/styles/**/*.sass', ['sass']);
-    // gulp.watch('/src/scripts/**/*.js'.js, ['js']);
-    gulp.watch('src/**/*.jade', ['jade']);
-    // gulp.watch('/src/*.php').on('change', reload);
-}); 
-
 // Compile Sass into CSS
 gulp.task('sass', function() {
     return gulp.src('src/styles/**/*.sass')
@@ -69,22 +54,35 @@ gulp.task('sass', function() {
         .pipe(reload({stream: true}));
 });
 
+// Compile Babel es6 to JS
+gulp.task("babel", function() {
+    return gulp.src("src/scripts/**/*.js")
+        // .pipe(plumber(plumberErrorHandler))
+        //     .pipe(jshint())
+        //     .pipe(jshint.reporter('fail'))
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('site.min.js'))
+        .pipe(uglify())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("app/dist/js/"))
+        .pipe(reload({stream:true}));
+})
 
+// Proxy Server + watch PHP sass and js
+gulp.task('serve', function() {
 
-// // JS Hint and Concat Javascript
-// // gulp.task('js', function () {
-// //     return gulp.src(src.js)
-// //         .pipe(plumber(plumberErrorHandler))
-// //         // .pipe(jshint())
-// //         // .pipe(jshint.reporter('fail'))
-// //         .pipe(sourcemaps.init())
-// //             .pipe(concat(src.js))
-// //             .pipe(uglify())
-// //             .pipe(rename('site.min.js'))
-// //         .pipe(sourcemaps.write('./'))
-// //         .pipe(gulp.dest(src.jsdist))
-// //         .pipe(reload({stream:true}));        
- 
-// // });
+    // browserSync({
+    //     proxy: "www.jp-gen-starter-bp.dev",
+    //     // tunnel: "genesis-bourbon"
+    // });
+
+    gulp.watch('src/styles/**/*.sass', ['sass']);
+    gulp.watch('/src/scripts/**/*.js', ['babel']);
+    gulp.watch('src/**/*.jade', ['jade']);
+    // gulp.watch('/src/*.php').on('change', reload);
+}); 
 
 gulp.task('default', ['serve']);
--- src/scripts/site.js
+++ src/scripts/site.js
@@ -1,5 +1,6 @@
 // jQuery noConflict Wrapper
 jQuery(document).ready(function($) {
 
-
+  let party = "hardy";
+  const hardy = "party";
 });

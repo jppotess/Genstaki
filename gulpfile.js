50ddf216bf32bb4f8827be3570918956aa73c78e
John Potess <jppotess@gmail.com>
2016-01-03 20:44:07 -0600

    removed scss configs for sass for now

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp         = require('gulp');
 
-
 var sourcemaps   = require('gulp-sourcemaps');
 var sass         = require('gulp-sass');
 var sassConvert  = require('sass-convert');
@@ -22,6 +21,10 @@ var rename       = require('gulp-rename');
 
 var browserSync  = require('browser-sync');
 var reload       = browserSync.reload;
+
+var cache        = require('gulp-cache');
+var imagemin     = require('gulp-imagemin');
+var del          = require('del');
 
 /**
  * Options Variables
@@ -48,6 +51,13 @@ var plumberErrorHandler = { errorHandler: notify.onError({
 //     templatesDist: '/testywesty'
 // };
 
+
+gulp.task('images', function(){
+    return gulp.src('app/dist/images/**/*.+(png|jpg|gif|svg)')
+    .pipe(cache(imagemin()))
+    .pipe(gulp.dest('app/dist/images'))
+})
+
 // Jade to PHP
 gulp.task('jade', function() {
      gulp.src('src/templates/**/*.jade')
@@ -60,58 +70,57 @@ gulp.task('jade', function() {
         .pipe(reload({stream: true}));
 });
 
-//Convert SCSS to Sass
-gulp.task('scss2sass', function() {
-    return gulp.src('src/styles/utilities/*.scss')
-        .pipe(sassConvert({
-            from: 'sass',
-            to:   'scss',
-        }))
-        // .pipe(concat('utilities.sass'))
-        // .pipe(gulp.dest('src/styles/'));
-        .pipe(gulp.dest('src/styles/utilities'));
-})
+// // //Convert SCSS to Sass
+// gulp.task('scss2sass', function() {
+//     return gulp.src('src/styles/utilities/*.scss')
+//         .pipe(sassConvert({
+//             from: 'scss',
+//             to:   'sass',
+//         }))
+//         // .pipe(concat('utilities.sass'))
+//         // .pipe(gulp.dest('src/styles/'));
+//         .pipe(gulp.dest('src/styles/utilities'));
+// })
 
 // Compile Sass into CSS
-gulp.task('sass', ['scss2sass'], function() {
+gulp.task('sass', function() {
     return gulp.src([
             'src/styles/utilities.sass',
             'src/styles/**/*.+(sass|scss)'
         ])
-        .pipe(plumber(plumberErrorHandler))
-        .pipe(sourcemaps.init())
-        .pipe(sass({indentedSyntax: 'true'}).on('error', sass.logError))
-        .pipe(concat('styles.min.css'))
-        // .pipe(minifyCss())
-        .pipe(autoprefixer(autoprefixerOptions))
-        .pipe(sourcemaps.write('./'))        
-        .pipe(gulp.dest('app/dist/css/'))
-        .pipe(reload({stream: true}));
+    .pipe(plumber(plumberErrorHandler))
+    .pipe(sourcemaps.init())
+    .pipe(sass({indentedSyntax: 'true'}).on('error', sass.logError))
+    .pipe(concat('styles.min.css'))
+    // .pipe(minifyCss())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(sourcemaps.write('./'))        
+    .pipe(gulp.dest('app/dist/css/'))
+    .pipe(reload({stream: true}));
 });
 
 // Compile Babel es6 to JS
 gulp.task("babel", function() {
     return gulp.src("src/scripts/**/*.js")
-        // .pipe(plumber(plumberErrorHandler))
-        //     .pipe(jshint())
-        //     .pipe(jshint.reporter('fail'))
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(concat('scripts.min.js'))
-        // .pipe(uglify())
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("app/dist/js/"))
-        .pipe(reload({stream:true}));
+    // .pipe(plumber(plumberErrorHandler))
+    //     .pipe(jshint())
+    //     .pipe(jshint.reporter('fail'))
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ['es2015']
+    }))
+    .pipe(concat('scripts.min.js'))
+    // .pipe(uglify())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("app/dist/js/"))
+    .pipe(reload({stream:true}));
 })
 
 // Proxy Server + watch PHP sass and js
 gulp.task('serve', function() {
 
     // browserSync({
-    //     proxy: "www.jp-gen-starter-bp.dev",
-    //     // tunnel: "genesis-bourbon"
+    //     proxy: "www.proxy.dev",
     // });
 
     gulp.watch('src/styles/**/*.sass', ['sass']);

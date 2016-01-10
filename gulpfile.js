8c58d163915589931330de000cb66f5625f6d2a6
John Potess <jppotess@gmail.com>
2016-01-09 22:09:45 -0600

    moved from .sass to .scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp         = require('gulp');
 var del          = require('del');
 var runSequence  = require('run-sequence');
+var merge        = require('merge2');
+
+var bourbon      = require('node-bourbon');
+var neat         = require('node-neat');
 
 var sourcemaps   = require('gulp-sourcemaps');
 var sass         = require('gulp-sass');
@@ -19,7 +23,7 @@ var concat       = require('gulp-concat');
 var uglify       = require('gulp-uglify');
 var plumber      = require('gulp-plumber');
 var notify       = require('gulp-notify');
-var rename       = require('gulp-rename');
+// var rename       = require('gulp-rename');
 
 var browserSync  = require('browser-sync');
 var reload       = browserSync.reload;
@@ -39,17 +43,10 @@ var plumberErrorHandler = { errorHandler: notify.onError({
     })
 };
 
+var path = {
+  globalImports: 'src/lib/_global-imports.scss'
+};
 
-// var src = {
-//     scss:   '/stylesheets/**/*.scss',
-//     cssdist: '/dist/css',
-//     js:     '/javascript/**/*.js',
-//     jsdist: '/dist/js',    
-//     // html:   '/',
-//     php:     '/*.php',
-//     templates:    '/templates/**/*.jade',
-//     templatesDist: '/testywesty'
-// };
 
 // Images
 gulp.task('images', function(){
@@ -76,34 +73,42 @@ gulp.task('jade', function() {
         .pipe(reload({stream: true}));
 });
 
-// // //Convert SCSS to Sass
-// gulp.task('scss2sass', function() {
-//     return gulp.src('src/styles/utilities/*.scss')
+// // Convert Sass to SCSS
+// gulp.task('sass2scss', function() {
+//     return gulp.src('src/styles/**/*.scss')
 //         .pipe(sassConvert({
-//             from: 'scss',
-//             to:   'sass',
+//             from: 'sass',
+//             to:   'scss',
 //         }))
-//         // .pipe(concat('utilities.sass'))
-//         // .pipe(gulp.dest('src/styles/'));
-//         .pipe(gulp.dest('src/styles/utilities'));
+//         .pipe(rename(function (path) {
+//             path.extname = ".scss"
+//         }))
+//         .pipe(concat('styles.min.scss'))
+//         // .pipe(minifyCss())
+//         .pipe(gulp.dest('src/styles/'));
 // })
 
 // Compile Sass into CSS
 gulp.task('sass', function() {
-    return gulp.src([
-            'src/styles/utilities.sass',
-            'src/styles/**/*.+(sass|scss)'
-        ])
+    // return gulp.src(['src/styles/**/*.scss', '!src/styles/utilities/_global-imports.scss'])
+    
+    return merge(
+        gulp.src(['src/styles/**/*.scss', '!src/styles/utilities/_global-imports.scss']),
+        gulp.src('src/styles/utilities/_global-imports.scss')
+    )
     .pipe(plumber(plumberErrorHandler))
     .pipe(sourcemaps.init())
-    .pipe(sass({indentedSyntax: 'true'}).on('error', sass.logError))
+    .pipe(sass({
+        includePaths: require('node-bourbon').includePaths,
+        includePaths: require('node-neat').includePaths
+    }))
     .pipe(concat('styles.min.css'))
     // .pipe(minifyCss())
     .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(sourcemaps.write('./'))        
+    .pipe(sourcemaps.write('.'))        
     .pipe(gulp.dest('app/css/'))
     .pipe(reload({stream: true}));
-});
+})
 
 // Compile Babel es6 to JS
 gulp.task("babel", function() {
@@ -122,35 +127,43 @@ gulp.task("babel", function() {
     .pipe(reload({stream:true}));
 })
 
-// Proxy Server + watch PHP sass and js
-gulp.task('serve',  ['build'], function() {
-
-    // browserSync({
-    //     proxy: "www.proxy.dev",
-    // });
-
-    gulp.watch('src/styles/**/*.sass', ['sass']);
-    gulp.watch('/src/scripts/**/*.js', ['babel']);
-    gulp.watch('src/**/*.jade', ['jade']);
-})
 
 // Clean Task
-gulp.task('clean', function(callback) {
+gulp.task('cleanFull', function(callback) {
     del('app');
     return cache.clearAll(callback);
 })
 
+
 // Clean Task - excpt images
-gulp.task('clean:dist', function(callback){
+gulp.task('clean', function(callback){
     del(['app/**/*', '!app/images', '!app/images/**/*'], callback)
 })
 
+
 // Build Task
 gulp.task('build', function(callback){
-    runSequence('clean:dist',
+    runSequence('clean',
         ['jade', 'sass', 'babel', 'images', 'fonts'],
         callback
     )
+})
+
+
+// Watch Task
+gulp.task('watch', ['build'], function() {
+    gulp.watch('src/styles/**/*.sass', ['sass']);
+    gulp.watch('src/scripts/**/*.js', ['babel']);
+    gulp.watch('src/**/*.jade', ['jade']);
+})
+
+
+// Proxy Server + watch PHP sass and js
+gulp.task('serve',  ['watch'], function() {
+
+    // browserSync({
+    //     proxy: "www.proxy.dev",
+    // });
 })
 
 
